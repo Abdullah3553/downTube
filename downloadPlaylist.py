3115fc11fee3c151899015b524fdb6cab1ee7d43
Abdullah Mahmoud <49757373+Abdullah3553@users.noreply.github.com>
2023-11-23 23:26:00 +0200

    Adding the script file :p

---
+++ downloadPlaylist.py
@@ -0,0 +1,29 @@
+
+# this is a really ^simple^ program to download a palylist from a youtube ... 
+
+import sys
+from pytube import Playlist
+
+def downloadPlaylist(playListURL, startingVid):
+    # playListURL is the url for the playlist you want to download
+    # startingVid is the number of the video you want t start from ( the first video is 1, the second is 2 etc... ) 
+    playList = Playlist(playListURL)
+    print(f'Downloading all videos in {playList.title} in highest resolution.')
+    print("DO NOT PANIC IF IT LOOKS STUCK. EVERYTHING IS FINE jsut be patient :D")
+    i = startingVid-1
+    while i < len(playList.videos):
+        try:
+            #playList.videos[i].streams.get_highest_resolution().download()
+            print(playList.videos[i].title)
+        except:
+            print(f"FAIL  >>>  {playList.videos[i].title }\nRetrying...")
+            continue
+        i = i+1
+	
+#URL = sys.argv[1] # first argument after the script name >> uncomment this to use terminal arguments
+#startingFrom = sys.argv[2] # the second argument after the script name >> uncomment this to use terminal arguments
+URL = input("Enter the URL of the playlist >_ ")
+startingFrom = int(input("Enter the starting video number ( writre 1 for the first video in the playlist)"))
+#print(f"URL is {URL}") # testing.. 
+#print(f"starting from is {startingFrom}") # testing..
+downloadPlaylist(URL, int(startingFrom))
