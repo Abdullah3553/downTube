0cee3ddad98dd0975b54660d9ccf29fa649f6a78
Abdullah Mahmoud <49757373+Abdullah3553@users.noreply.github.com>
2023-11-23 23:33:21 +0200

    LOL !!
    the downloading command was commented LOL !!

--- downloadPlaylist.py
+++ downloadPlaylist.py
@@ -13,17 +13,19 @@ def downloadPlaylist(playListURL, startingVid):
     i = startingVid-1
     while i < len(playList.videos):
         try:
-            #playList.videos[i].streams.get_highest_resolution().download()
-            print(playList.videos[i].title)
+            print(f"downloading >> {playList.videos[i].title} ")
+            playList.videos[i].streams.get_highest_resolution().download()
+            print(f"{playList.videos[i].title} >> Downloaded. ")
         except:
-            print(f"FAIL  >>>  {playList.videos[i].title }\nRetrying...")
+            print(f"FAIL  >>>  {playList.videos[i].title }\nRetrying...(Press Ctrl + C to stop the script)")
             continue
         i = i+1
-	
+	print("The playlist has been downloaded !!")
+    return 0 
 #URL = sys.argv[1] # first argument after the script name >> uncomment this to use terminal arguments
 #startingFrom = sys.argv[2] # the second argument after the script name >> uncomment this to use terminal arguments
 URL = input("Enter the URL of the playlist >_ ")
 startingFrom = int(input("Enter the starting video number ( writre 1 for the first video in the playlist)"))
 #print(f"URL is {URL}") # testing.. 
 #print(f"starting from is {startingFrom}") # testing..
-downloadPlaylist(URL, int(startingFrom))
+downloadPlaylist(URL, startingFrom)
